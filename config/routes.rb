5d209986e7ed9183150a465a1c2337e804d3a546
Pierre Waechter <pierre.waechter@audencia.com>
2020-08-13 13:09:59 +0200

    doses controller generated + routes updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: %i[index show new create]
+  resources :doses, only: :destroy
+
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[new create]
+  end
 end

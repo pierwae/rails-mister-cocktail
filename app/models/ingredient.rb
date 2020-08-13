95226cdfa4c90eb03339a3e655d239e05ebab66e
Pierre Waechter <pierre.waechter@audencia.com>
2020-08-13 12:26:53 +0200

    cocktail - dose - ingredient models ready  and rspec spec/models 100% green

---
+++ app/models/ingredient.rb
@@ -0,0 +1,12 @@
+class Ingredient < ApplicationRecord
+  has_many :doses
+  validates :name, presence: true, uniqueness: true
+  before_destroy :can_be_destroyed?
+
+  private
+
+  def can_be_destroyed?
+    errors.add(:base, 'cannot delete ingredient used by a cocktail') unless doses.count.zero?
+    errors.blank?
+  end
+end
